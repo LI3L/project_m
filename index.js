d8d3b776a7a34e87b140a90f85855411a13b6f32
LI3L <khtkvg@gmail.com>
2023-05-14 14:52:54 +0300

    7

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/",action);
 database
 .sync()
 .then(result=>{
-    console.log(results);
+    console.log(result);
     app.listen(process.env.PORT,()=>
     console.log(`Server running on port ${process.env.PORT}`));
 })
